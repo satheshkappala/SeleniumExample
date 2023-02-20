558abaeb4fcec2fa57951c7a6437876b7c72bfdf
kappala sathesh <satheshkappala@gmail.com>
2023-02-20 19:53:18 +0530

    button text

---
+++ src/test/java/Test/ButtonTest.java
@@ -0,0 +1,34 @@
+package Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class ButtonTest {
+	
+	
+WebDriver driver;
+	
+	@BeforeClass
+	void launchBrowser() {
+		System.setProperty("webdriver.chrome.driver", "E:\\\\Selenium jatfiles\\\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.mycontactform.com/");
+		driver.manage().window().maximize();
+	}
+	@Test
+	void buttonMethods() {
+		
+		
+	}
+	
+	
+
+	@AfterClass
+	public void closeBrowser() {
+		driver.quit(); //Terminate the browser session
+	}
+
+}
