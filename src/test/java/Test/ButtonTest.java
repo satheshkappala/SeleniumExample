17b9c3f40908cae5c4d16b8c1ddc85607101fef9
kappa <satheshkappala@gmail.com>
2023-02-21 13:40:13 +0530

    button text added

--- src/test/java/Test/ButtonTest.java
+++ src/test/java/Test/ButtonTest.java
@@ -1,7 +1,12 @@
 package Test;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -20,13 +25,32 @@ WebDriver driver;
 	}
 	@Test
 	void buttonMethods() {
-		System.out.println("button methods");
+		WebElement btnlogin = driver.findElement(By.name("btnSubmit"));
+		System.out.println(btnlogin.isDisplayed());
+		System.out.println(btnlogin.isEnabled());
+		System.out.println(btnlogin.getAttribute("name"));
+		System.out.println(btnlogin.getAttribute("type"));
+		System.out.println(btnlogin.getAttribute("class"));
+		System.out.println(btnlogin.getAttribute("value "));
+		System.out.println(btnlogin.getAttribute("tabindex"));
+		System.out.println(btnlogin.getTagName());
+		
+		//enter the data
+		btnlogin.click();
 		
 		
-	}
+		driver.navigate().to("https://www.mycontactform.com/samples.php");
+		List<WebElement> elements = driver.findElements(By.xpath("//input[@type='text']"));
+		System.out.println("No of webelement :"+elements.size());
+		//lambda expression
+	//	elements.forEach(element->System.out.println(element.getAttribute("name")));
+		elements.forEach(element->element.sendKeys("testing"));
+		
+	//	stream api
+	long noofobjects = elements.stream().filter(element->element.getAttribute("type").equals("text")).count();
+		System.out.println("No of text fields are :"+noofobjects);
+	}  
 	
-	
-
 	@AfterClass
 	public void closeBrowser() {
 		driver.quit(); //Terminate the browser session
